45a7a271e47433d18f5573146ce1044d8c266268
331323 <73833779+331323@users.noreply.github.com>
2022-09-22 10:53:47 +0200

    feat
    added a "feel gestelde vragen" page with a new footer in the index page
    
    in the question page vue, I'm still thinkin if ill use the color or  only the picture in the back

---
+++ pages/question.vue
@@ -0,0 +1,239 @@
+<template>
+ <div>
+     <OzbBackground />
+     <!-- <img class="background__bridge" src="~/assets/img/open-zaakbrug-brug.svg" /> -->
+     <nav class="navbar navbar-light bg-transparant">
+            <a class="navbar-brand" href="http://10.0.0.165:3000/">
+                <img class="logo" src="~/assets/img/logo-open-zaakbrug.svg" alt="Zaakbrug logo" />
+            </a>
+     </nav>
+
+     <section>
+            <h2>Met wat kunnen we u helpen?</h2>
+     </section>
+
+     <section class="section1">
+         <div class="container">
+              <div class="row mt-6 mb-5">
+                <div class="col">
+                    <details class="details">
+                    <summary>What is Lorem Ipsum?</summary>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        </p>
+                    </div>
+                </details>
+                <details class="details">
+                    <summary>Why do we use it?</summary>
+                    <div class="description">
+                        <p >
+                         Id cursus metus aliquam eleifend mi in nulla posuere. Fermentum et sollicitudin ac orci phasellus egestas. Tempus iaculis urna id volutpat lacus laoreet non. Sit amet tellus cras adipiscing enim eu. Cursus in hac habitasse platea dictumst quisque sagittis. Tempus egestas sed sed risus. Mauris nunc congue nisi vitae suscipit tellus. Faucibus nisl tincidunt eget nullam non. At tempor commodo ullamcorper a. Donec ac odio tempor orci dapibus. Sed egestas egestas fringilla phasellus faucibus scelerisque. Condimentum mattis pellentesque id nibh tortor. Sit amet dictum sit amet justo donec enim diam. Mi ipsum faucibus vitae aliquet nec ullamcorper. Pellentesque elit eget gravida cum sociis. Est sit amet facilisis magna etiam tempor.
+                        </p>
+                    </div>
+                </details>
+                <details class="details">
+                    <summary>Hoe werkt het</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>waar kan ik contact nemen</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>algemeen</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>hello</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+                </div>
+
+
+                <div class="col-12 col-md-5 col-xl-5">
+                    <details class="details">
+                    <summary>Wat is openzaak burg</summary>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl suscipit adipiscing bibendum est ultricies integer. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Cras tincidunt lobortis feugiat vivamus at augue eget. Libero justo laoreet sit amet cursus sit amet dictum. Fermentum dui faucibus in ornare quam viverra orci sagittis. Diam vel quam elementum pulvinar etiam. Risus in hendrerit gravida rutrum quisque non tellus. Vel pretium lectus quam id leo in vitae. Ut eu sem integer vitae justo. Lectus urna duis convallis convallis tellus id interdum velit. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Hac habitasse platea dictumst vestibulum rhoncus est. Ac auctor augue mauris augue neque gravida in fermentum. Proin sed libero enim sed faucibus. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Tempus iaculis urna id volutpat lacus laoreet.
+                        </p>
+                    </div>
+                </details>
+                <details class="details">
+                    <summary>wie heeft het gemaakt</summary>
+                    <div class="description">
+                        <p>
+                            Id cursus metus aliquam eleifend mi in nulla posuere. Fermentum et sollicitudin ac orci phasellus egestas. Tempus iaculis urna id volutpat lacus laoreet non. Sit amet tellus cras adipiscing enim eu. Cursus in hac habitasse platea dictumst quisque sagittis. Tempus egestas sed sed risus. Mauris nunc congue nisi vitae suscipit tellus. Faucibus nisl tincidunt eget nullam non. At tempor commodo ullamcorper a. Donec ac odio tempor orci dapibus. Sed egestas egestas fringilla phasellus faucibus scelerisque. Condimentum mattis pellentesque id nibh tortor. Sit amet dictum sit amet justo donec enim diam. Mi ipsum faucibus vitae aliquet nec ullamcorper. Pellentesque elit eget gravida cum sociis. Est sit amet facilisis magna etiam tempor.
+                        </p>
+                    </div>
+                </details>
+                <details class="details">
+                    <summary>Hoe werkt het</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>waar kan ik contact nemen</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>algemeen</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>perferendis aperiam voluptatem consequuntur molestiae ratione</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+                </div>
+              </div>
+           </div>
+      </section>
+
+      <section class="section1">
+         <div class="container">
+              <div class="row mt-6 mb-5">
+                <div class="col">
+                    <details class="details">
+                    <summary>Wat is openzaak burg</summary>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl suscipit adipiscing bibendum est ultricies integer. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Cras tincidunt lobortis feugiat vivamus at augue eget. Libero justo laoreet sit amet cursus sit amet dictum. Fermentum dui faucibus in ornare quam viverra orci sagittis. Diam vel quam elementum pulvinar etiam. Risus in hendrerit gravida rutrum quisque non tellus. Vel pretium lectus quam id leo in vitae. Ut eu sem integer vitae justo. Lectus urna duis convallis convallis tellus id interdum velit. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Hac habitasse platea dictumst vestibulum rhoncus est. Ac auctor augue mauris augue neque gravida in fermentum. Proin sed libero enim sed faucibus. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Tempus iaculis urna id volutpat lacus laoreet.
+                        </p>
+                    </div>
+                </details>
+                <details class="details">
+                    <summary>wie heeft het gemaakt</summary>
+                    <div class="description">
+                        <p >
+                         Id cursus metus aliquam eleifend mi in nulla posuere. Fermentum et sollicitudin ac orci phasellus egestas. Tempus iaculis urna id volutpat lacus laoreet non. Sit amet tellus cras adipiscing enim eu. Cursus in hac habitasse platea dictumst quisque sagittis. Tempus egestas sed sed risus. Mauris nunc congue nisi vitae suscipit tellus. Faucibus nisl tincidunt eget nullam non. At tempor commodo ullamcorper a. Donec ac odio tempor orci dapibus. Sed egestas egestas fringilla phasellus faucibus scelerisque. Condimentum mattis pellentesque id nibh tortor. Sit amet dictum sit amet justo donec enim diam. Mi ipsum faucibus vitae aliquet nec ullamcorper. Pellentesque elit eget gravida cum sociis. Est sit amet facilisis magna etiam tempor.
+                        </p>
+                    </div>
+                </details>
+                <details class="details">
+                    <summary>Hoe werkt het</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>waar kan ik contact nemen</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>algemeen</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>hello</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+                </div>
+
+
+                <div class="col-12 col-md-5 col-xl-5">
+                    <details class="details">
+                    <summary>Wat is openzaak burg</summary>
+                    <div class="description">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl suscipit adipiscing bibendum est ultricies integer. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Cras tincidunt lobortis feugiat vivamus at augue eget. Libero justo laoreet sit amet cursus sit amet dictum. Fermentum dui faucibus in ornare quam viverra orci sagittis. Diam vel quam elementum pulvinar etiam. Risus in hendrerit gravida rutrum quisque non tellus. Vel pretium lectus quam id leo in vitae. Ut eu sem integer vitae justo. Lectus urna duis convallis convallis tellus id interdum velit. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Hac habitasse platea dictumst vestibulum rhoncus est. Ac auctor augue mauris augue neque gravida in fermentum. Proin sed libero enim sed faucibus. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Tempus iaculis urna id volutpat lacus laoreet.
+                        </p>
+                    </div>
+                </details>
+                <details class="details">
+                    <summary>wie heeft het gemaakt</summary>
+                    <div class="description">
+                        <p>
+                            Id cursus metus aliquam eleifend mi in nulla posuere. Fermentum et sollicitudin ac orci phasellus egestas. Tempus iaculis urna id volutpat lacus laoreet non. Sit amet tellus cras adipiscing enim eu. Cursus in hac habitasse platea dictumst quisque sagittis. Tempus egestas sed sed risus. Mauris nunc congue nisi vitae suscipit tellus. Faucibus nisl tincidunt eget nullam non. At tempor commodo ullamcorper a. Donec ac odio tempor orci dapibus. Sed egestas egestas fringilla phasellus faucibus scelerisque. Condimentum mattis pellentesque id nibh tortor. Sit amet dictum sit amet justo donec enim diam. Mi ipsum faucibus vitae aliquet nec ullamcorper. Pellentesque elit eget gravida cum sociis. Est sit amet facilisis magna etiam tempor.
+                        </p>
+                    </div>
+                </details>
+                <details class="details">
+                    <summary>Hoe werkt het</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>waar kan ik contact nemen</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>algemeen</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+              <details class="details">
+                    <summary>perferendis aperiam voluptatem consequuntur molestiae ratione</summary>
+                    <div class="description">
+                        <p>Tenetur, ex delectus, perferendis aperiam voluptatem consequuntur molestiae ratione  rerum vitae ab    modi, minus placeat quis dignissimos. Dolorem quaerat odit, iusto laboriosam possimus, in architecto aliquam commodi sapiente saepe sequi at eligendi hic reprehenderit repellendus quos!</p>
+                    </div>
+              </details>
+                </div>
+              </div>
+           </div>
+      </section>
+ </div>
+</template>
+
+<style scoped >
+
+.logo {
+    width: 100%;
+    max-width: 150px
+    
+}
+
+h2{
+    text-align: center;
+    text-transform: none;
+    padding: 50px;
+    font-weight: 700;
+    font-size: 1.5em;
+}
+
+.section1{
+    padding: 10px;
+}
+
+
+
+.details{
+ background-color: transparent;
+  color: black;
+}
+
+.active, .details:hover {
+    padding: 0;
+ background-color:  rgba(0, 255, 195, 0.298);
+  color:  black; 
+}
+
+p{
+    padding: 10px 50px 20px;
+}
+</style>
