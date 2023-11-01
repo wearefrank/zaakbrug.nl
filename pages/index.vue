75b5665f1a0f5cc2de73b70998d54f115d29f065
Sergi Philipsen <philipsen.sergi@gmail.com>
2023-11-01 13:33:06 +0100

    feat: add technical links to the index page

--- pages/index.vue
+++ pages/index.vue
@@ -75,6 +75,15 @@ Stap over van de oude Zaak- en Documentservices (ZDS) naar de Zaakgericht Werken
               <li>
                 <a href="https://wearefrank.tv/watch/135">🎥 [Video] - Wat is Open Zaakbrug en hoe past het binnen Common Ground?</a>
               </li>
+              <li>
+                <a href="https://github.com/wearefrank/zaakbrug">🐙 [Bron] - Broncode van de ZaakBrug op GitHub</a>
+              </li>
+              <li>
+                <a href="https://wearefrank.github.io/charts/zaakbrug">🔀 [Applicatie] - Haven component (Helm CHart) voor het draaien van de ZaakBrug</a>
+              </li>
+              <li>
+                <a href="https://hub.docker.com/r/wearefrank/zaakbrug">🐳 [Applicatie] - Container image voor het draaien van de ZaakBrug in een Docker omgeving</a>
+              </li>
             </ul>
           </div>
         </div>
